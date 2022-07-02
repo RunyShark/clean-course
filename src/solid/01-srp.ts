a83c1d1bbfb35afb0c0dc2e967ec6e068f73117d
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-02 12:34:55 -0500

    Solid

--- src/solid/01-srp.ts
+++ src/solid/01-srp.ts
@@ -4,33 +4,43 @@
     name: string;
   }
 
-  // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
-  // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
-  class ProductBloc {
+  class ProductService {
     loadProduct(id: number) {
-      // Realiza un proceso para obtener el producto y retornarlo
       console.log("Producto: ", { id, name: "OLED Tv" });
     }
-
     saveProduct(product: Product) {
-      // Realiza una petición para salvar en base de datos
       console.log("Guardando en base de datos", product);
     }
+  }
 
-    notifyClients() {
+  class ProductBloc {
+    loadProduct(id: number) {
+      console.log("Producto: ", { id, name: "OLED Tv" });
+    }
+    saveProduct(product: Product) {
+      console.log("Guardando en base de datos", product);
+    }
+  }
+
+  class Mailer {
+    sendEmail() {
       console.log("Enviando correo a los clientes");
     }
+  }
 
-    onAddToCart(productId: number) {
-      // Agregar al carrito de compras
+  class CartBloc {
+    private itemsInCart: Object[] = [];
+    AddToCart(productId: number) {
       console.log("Agregando al carrito ", productId);
     }
   }
 
   const productBloc = new ProductBloc();
-
-  productBloc.loadProduct(10);
-  productBloc.saveProduct({ id: 10, name: "OLED TV" });
-  productBloc.notifyClients();
-  productBloc.onAddToCart(10);
+  const cartBloc = new CartBloc();
+  const productService = new ProductService();
+  const mailer = new Mailer();
+  productService.loadProduct(10);
+  productService.saveProduct({ id: 10, name: "OLED TV" });
+  mailer.sendEmail();
+  cartBloc.AddToCart(10);
 })();
