130620bfda64f3bd428804c2a40d51228e4a5a78
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-01 20:25:02 -0500

    SOLID

---
+++ src/clean-code/01-names.ts
@@ -0,0 +1,45 @@
+(() => {
+  // Ejemplo
+  // Archivos a evaluar - files to evaluate
+  const filesToEvaluate = [
+    { id: 1, evaluate: false },
+    { id: 2, evaluate: false },
+    { id: 3, evaluate: true },
+    { id: 4, evaluate: false },
+    { id: 5, evaluate: false },
+    { id: 7, evaluate: true },
+  ];
+
+  // Archivos marcados para borrar - files to delete
+  const filesToDelete = filesToEvaluate.map((files) => files.evaluate);
+
+  class User {}
+  class UserMixin {}
+  class UserImplementation {}
+  interface User {}
+
+  // Mejor
+
+  // Todo: Tarea
+
+  // día de hoy - today
+  const toDay = new Date();
+
+  // días transcurridos - elapsed time in days
+  const delapsedTimeInDays: number = 23;
+
+  // número de archivos en un directorio - number of files in directory
+  const numberFilesDirectory = 33;
+
+  // primer nombre - first name
+  const firstName = "Fernando";
+
+  // primer apellido - last name
+  const lastName = "Herrera";
+
+  // días desde la última modificación - days since modification
+  const lastDayModification = 12;
+
+  // cantidad máxima de clases por estudiante - max classes per student
+  const maxClassForStudents = 6;
+})();
