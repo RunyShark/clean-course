12618e9299bdb6314f67c493bb6696fcb43648cd
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-01 22:02:19 -0500

    04

--- src/clean-code/03-funciones.ts
+++ src/clean-code/03-funciones.ts
@@ -31,4 +31,16 @@
     console.log("Crear actor");
     return true;
   }
+
+  const getPayAmount = ({
+    isDead = false,
+    isSeparated = true,
+    isRetired = false,
+  }): Number => {
+    if (isDead) return 1500;
+
+    if (isSeparated) return 2500;
+
+    return isRetired ? 3000 : 4000;
+  };
 })();
