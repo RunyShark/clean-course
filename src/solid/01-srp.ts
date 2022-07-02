57c7b809de8877ec477dd7510500d95b377ab76e
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-02 12:41:00 -0500

    solid

--- src/solid/01-srp.ts
+++ src/solid/01-srp.ts
@@ -5,26 +5,35 @@
   }
 
   class ProductService {
-    loadProduct(id: number) {
+    getProduct(id: number) {
       console.log("Producto: ", { id, name: "OLED Tv" });
     }
     saveProduct(product: Product) {
       console.log("Guardando en base de datos", product);
     }
   }
-
-  class ProductBloc {
-    loadProduct(id: number) {
-      console.log("Producto: ", { id, name: "OLED Tv" });
-    }
-    saveProduct(product: Product) {
-      console.log("Guardando en base de datos", product);
-    }
-  }
-
   class Mailer {
     sendEmail() {
       console.log("Enviando correo a los clientes");
+    }
+  }
+  class ProductBloc {
+    private productService: ProductService;
+    private mailer: Mailer;
+
+    constructor(productService: ProductService, mailer: Mailer) {
+      this.productService = productService;
+      this.mailer = mailer;
+    }
+
+    loadProduct(id: number) {
+      this.productService.getProduct(id);
+    }
+    saveProduct(product: Product) {
+      this.productService.saveProduct(product);
+    }
+    notifyClients() {
+      this.mailer.sendEmail();
     }
   }
 
@@ -35,12 +44,14 @@
     }
   }
 
-  const productBloc = new ProductBloc();
-  const cartBloc = new CartBloc();
   const productService = new ProductService();
   const mailer = new Mailer();
-  productService.loadProduct(10);
-  productService.saveProduct({ id: 10, name: "OLED TV" });
-  mailer.sendEmail();
+
+  const productBloc = new ProductBloc(productService, mailer);
+  const cartBloc = new CartBloc();
+
+  productBloc.loadProduct(10);
+  productBloc.saveProduct({ id: 10, name: "OLED TV" });
+  productBloc.notifyClients();
   cartBloc.AddToCart(10);
 })();
